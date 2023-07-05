5fe3e3db33b57cba4afb0234d0b0c572f5fcfb8a
sushiiiii09 <sshreechoudhury@gmail.com>
2023-07-05 23:56:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import background from "./images/background.jpeg";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${background})`,
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div className="px-10 pt-10 ">
+        <Sidebar />
+        <div className="z-0">
+          <h1 className="text-4xl mt-10 text-white text-center">main</h1>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,206 @@
+import React, { useState } from "react";
+
+export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      {!isOpen ? (
+        <button
+          className="w-[50px] h-[50px] fixed items-center cursor-pointer left-6 top-6"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <svg
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#E3DFFD"
+            stroke="#E3DFFD"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <g id="Complete">
+                <g id="align-left">
+                  <g>
+                    <polygon
+                      fill="#ffffff"
+                      points="12.9 18 4.1 18 4.1 18 12.9 18 12.9 18"
+                      stroke="#E3DFFD"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                    ></polygon>{" "}
+                    <polygon
+                      fill="#ffffff"
+                      points="20 14 4 14 4 14 20 14 20 14"
+                      stroke="#E3DFFD"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                    ></polygon>{" "}
+                    <polygon
+                      fill="#ffffff"
+                      points="12.9 10 4.1 10 4.1 10 12.9 10 12.9 10"
+                      stroke="#E3DFFD"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                    ></polygon>{" "}
+                    <polygon
+                      fill="#ffffff"
+                      points="20 6 4 6 4 6 20 6 20 6"
+                      stroke="#E3DFFD"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                    ></polygon>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
+        </button>
+      ) : (
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="text-white fixed left-7 top-6 z-10 w-[60px] h-[60px]"
+        >
+          <svg
+            width="40px"
+            height="40px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <path
+                d="M19 5L5 19M5 5L9.5 9.5M12 12L19 19"
+                stroke="#ffff"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>{" "}
+            </g>
+          </svg>
+        </button>
+      )}
+      <div
+        className={`top-0 left-0 fixed bg-purple-950 bg-opacity-80 md:w-[25vw] lg:w-[30vw] h-full sm:w-[50vw] p-20 
+    ${
+      isOpen ? "translate-x-0" : "-translate-x-full"
+    } ease-in-out duration-300 `}
+      >
+        <div className=" py-6">
+          <span className="grid h-10 w-32 place-content-center rounded-md opacity-100 bg-white text-xs text-gray-900">
+            Logo
+          </span>
+
+          <ul className=" opacity-100 mt-6 space-y-1">
+            <li>
+              <a
+                href="https://tailwindcss.com/docs/customizing-colors"
+                className="block opacity-100 px-4 py-2 text-lg font-medium text-white hover:border-b hover:text-purple-500 "
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://tailwindcss.com/docs/customizing-colors"
+                className="block  px-4 py-2 text-lg font-medium text-white hover:border-b hover:text-purple-500 "
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://tailwindcss.com/docs/customizing-colors"
+                className="block  px-4 py-2 text-lg font-medium  text-white hover:border-b hover:text-purple-500 "
+              >
+                Contact Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://tailwindcss.com/docs/customizing-colors"
+                className="block  px-4 py-2 text-lg font-medium  text-white hover:border-b hover:text-purple-500 "
+              >
+                Upcoming events
+              </a>
+            </li>
+
+            <li>
+              <details className="group [&_summary::-webkit-details-marker]:hidden">
+                <summary className="flex cursor-pointer items-center justify-between text-white px-4 py-2 hover:border-b hover:text-purple-500 ">
+                  <span className="text-lg font-medium ">Domains</span>
+
+                  <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <ul className="mt-2 space-y-1 px-4">
+                  <li>
+                    <a
+                      href="https://tailwindcss.com/docs/customizing-colors"
+                      className="block px-4 py-2 text-md font-medium text-white hover:border-b hover:text-purple-500 "
+                    >
+                      Blockchain
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      href="https://tailwindcss.com/docs/customizing-colors"
+                      className="block  px-4 py-2 text-md font-medium text-white hover:border-b hover:text-purple-500 "
+                    >
+                      Web-Development
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://tailwindcss.com/docs/customizing-colors"
+                      className="block  px-4 py-2 text-md font-medium text-white hover:border-b hover:text-purple-500 "
+                    >
+                      Creatives
+                    </a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+
+            <li>
+              <a
+                href="https://tailwindcss.com/docs/customizing-colors"
+                className="block  px-4 py-2 text-md font-large font-medium text-white hover:border-b hover:text-purple-500 "
+              >
+                Privacy Policy
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
